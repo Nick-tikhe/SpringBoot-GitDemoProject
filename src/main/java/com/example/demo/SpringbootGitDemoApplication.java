5ff3c7d15654d6abc5ac2756fe7fe1c4a50066b1
Nick-tikhe <163338725+Nick-tikhe@users.noreply.github.com>
2024-03-15 15:30:12 +0530

    Update SpringbootGitDemoApplication.java

--- src/main/java/com/example/demo/SpringbootGitDemoApplication.java
+++ src/main/java/com/example/demo/SpringbootGitDemoApplication.java
@@ -14,6 +14,7 @@ public class SpringbootGitDemoApplication {
 	public String createProduct(@PathVariable String userName,@PathVariable double amount,@PathVariable String productName) {
 		return "user name is "+userName+" product name  "+productName+" product amount"+amount;
 	}
+	@GetMapping("/gethi")
 public String justtp(){
 	return "Hi";}
 	public static void main(String[] args) {
